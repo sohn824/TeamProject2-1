6c0e6a81be09b768c932583555ef75dd36396268
diligentStudent <ouosssssssa@gmail.com>
2018-05-01 08:32:27 +0900

    Ellen Acceleration
    shift를 눌러 엘렌 가속화

---
+++ Accelate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamekit2D
+{
+
+    public class Accelate : MonoBehaviour
+    { [SerializeField]
+        private GameObject ellen;
+
+        // Use this for initialization
+        public float speed;
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (Input.GetKey(KeyCode.LeftShift) == true && ellen.GetComponent<PlayerCharacter>().maxSpeed < 20f)
+            {
+                ellen.GetComponent<PlayerCharacter>().maxSpeed += Time.deltaTime*3;
+            }
+            else
+            {
+                if (ellen.GetComponent<PlayerCharacter>().maxSpeed > 10f)
+                    ellen.GetComponent<PlayerCharacter>().maxSpeed -= Time.deltaTime*3;
+            }
+        }
+
+    }
+}
